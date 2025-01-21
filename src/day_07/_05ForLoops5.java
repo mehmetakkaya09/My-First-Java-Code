17c064afbe8100e7f65790e837810b7da2ab9cfb
peace <mehmetakkaya1990@gmail.com>
2025-01-21 22:45:49 +0300

    for loops

--- src/day_07/_05ForLoops5.java
+++ src/day_07/_05ForLoops5.java
@@ -5,10 +5,14 @@ public class _05ForLoops5 {
 
         // erstellen Sie ein For Loop und geben Sie gerade Zahlen von 100 bis 0 aus
 
-        for (int i = 100; i >= 0 ; i--) {
+        for (int i = 100; i >= 0; i--) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
+        }
+
+        for (int i = 100; i >= 0; i -= 2) {
+            System.out.println(i);
         }
 
     }
