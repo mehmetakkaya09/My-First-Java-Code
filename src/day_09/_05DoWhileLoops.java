8236e7e3ad2bc1533ba138686c876ed4357a0540
peace <mehmetakkaya1990@gmail.com>
2025-01-23 22:13:01 +0300

    while and do - while loop

---
+++ src/day_09/_05DoWhileLoops.java
@@ -0,0 +1,39 @@
+package day_09;
+
+import java.util.Scanner;
+
+public class _05DoWhileLoops {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+
+        do {
+            System.out.print("Wie alt sind Sie : ");
+            age = sc.nextInt();
+        } while (age < 18);
+
+
+//        System.out.print("Wie alt sind Sie : ");
+//        age = sc.nextInt();
+//
+//        while (age < 18) {
+//            System.out.print("Wie alt sind Sie : ");
+//            age = sc.nextInt();
+//        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/day_09/_06ZahlenRateSpiel.java
@@ -0,0 +1,29 @@
+package day_09;
+
+public class _06ZahlenRateSpiel {
+
+    public static void main(String[] args) {
+
+        //Schreiben Sie ein Zahlenratespiel in Java, das folgende Bedingungen erfüllt:
+        //Das Programm wählt eine zufällige Zahl zwischen 1 und 100 aus.
+        //Der Benutzer muss diese Zahl erraten.
+        //Nach jedem falschen Tipp gibt das Programm einen Hinweis:
+        //Falls die eingegebene Zahl größer ist als die zufällige Zahl, soll die Meldung "Die Zahl, die Sie getippt haben, ist größer als meine Zahl." erscheinen.
+        //Falls die eingegebene Zahl kleiner ist als die zufällige Zahl, soll die Meldung "Die Zahl, die Sie getippt haben, ist kleiner als meine Zahl." erscheinen.
+        //Sobald der Benutzer die richtige Zahl erraten hat, gibt das Programm aus:
+        //"Herzlichen Glückwunsch! Sie haben richtig getippt."
+        //Das Programm soll auch anzeigen, wie viele Versuche der Benutzer benötigt hat.
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
