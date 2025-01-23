23864203a7522901ded6d3bf2b0579ca7c0abca8
peace <mehmetakkaya1990@gmail.com>
2025-01-23 21:46:47 +0300

    while and do - while loop

--- src/day_09/_03WhileLoopExercises.java
+++ src/day_09/_03WhileLoopExercises.java
@@ -1,5 +1,7 @@
 package day_09;
 
+import java.util.Scanner;
+
 public class _03WhileLoopExercises {
 
     public static void main(String[] args) {
@@ -7,15 +9,33 @@ public class _03WhileLoopExercises {
         // Sammeln Sie Zahlen zwischen 1 - 10, indem Sie While loop verwenden.
 
 
+        // Schreiben Sie ein Programm, das die Zahlen addiert, bis die Zahl 0 über die Tastatur eingegeben wird.
+        // 3 20 4 1 0    28
+
+        int sum = 0;
+        int number = 0;
+        boolean continueLoop = true;
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
 
 
+        while (continueLoop) {
+            count++;
+            System.out.print("Geben Sie " + count + ". Zahl : ");
+            number = sc.nextInt();
 
+            if (number == 0) {
+                continueLoop = false;
+            }
 
+            sum += number;
 
+            if (count % 3 == 0) {
+                System.out.println("inner sum = " + sum);
+            }
+        }
 
-
-
-
+        System.out.println("sum = " + sum);
 
     }
 
---
+++ src/day_09/_04EvenOddWhileEx.java
@@ -0,0 +1,19 @@
+package day_09;
+
+public class _04EvenOddWhileEx {
+
+    public static void main(String[] args) {
+
+        // Addieren Sie gerade und ungerade Zahlen getrennt zwischen 1 - 100
+
+        int geradeSum = 0;
+        int ungeradeSum = 0;
+
+
+
+
+
+    }
+
+
+}
