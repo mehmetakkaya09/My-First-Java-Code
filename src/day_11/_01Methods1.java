5e8be83a23b8458243354fe77db2312ee50c1f3b
peace <mehmetakkaya1990@gmail.com>
2025-01-27 21:06:26 +0300

    methods 1

---
+++ src/day_11/_01Methods1.java
@@ -0,0 +1,36 @@
+package day_11;
+
+public class _01Methods1 {
+
+    public static void main(String[] args) {
+
+
+        // DRY
+        // DON'T REPEAT YOURSELF
+
+
+    }
+
+    public static void sorting() {
+        System.out.println("Sorting Function has 200 codes");
+    }
+
+}
+
+class Baby {
+    public static void main(String[] args) {
+        _01Methods1.sorting();
+    }
+}
+
+class Home_Living {
+    public static void main(String[] args) {
+        _01Methods1.sorting();
+    }
+}
+
+class Shoes {
+    public static void main(String[] args) {
+        _01Methods1.sorting();
+    }
+}
