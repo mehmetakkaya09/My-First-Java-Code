03eb37c794e91314522dda74543938d36cce361d
peace <mehmetakkaya1990@gmail.com>
2025-01-27 23:18:23 +0300

    methods 2

---
+++ src/day_11/_06Parameters.java
@@ -0,0 +1,55 @@
+package day_11;
+
+public class _06Parameters {
+
+    public static void main(String[] args) {
+
+        biggerNumber(20, 5);
+        biggerNumber(5, 10);
+
+        //biggerNumber("mehmet", 20);
+
+        //biggerNumber(100.5, 12);
+
+        //biggerNumber(false, 11);
+
+        short number1 = 1;
+        byte number2 = 2;
+
+        biggerNumber(number1, number2); //  implicit casting
+
+        long number3 = 3;
+        long number4 = 4;
+
+        biggerNumber((int) number3, (int) number4);
+
+
+        boolean isEligibleToVote = false;
+
+        vote(isEligibleToVote);
+
+        vote(true);
+
+
+
+
+
+    }
+
+    public static void biggerNumber(int a, int b) {
+        if (a < b) {
+            System.out.println(b + " is greater than " + a);
+        } else
+            System.out.println(a + " is greater than " + b);
+    }
+
+    public static void vote(boolean isEligible){
+        if(isEligible){
+            System.out.println("Diese Person ist wahlberechtigt");
+        }else {
+            System.out.println("Diese Person ist nicht wahlberechtigt");
+        }
+    }
+
+
+}
---
+++ src/day_11/_07SumOfEvensAndOdds.java
@@ -0,0 +1,20 @@
+package day_11;
+
+public class _07SumOfEvensAndOdds {
+
+    public static void main(String[] args) {
+
+        // 2 method schreiben
+
+        // erste method wird ein int Zahl bekommen und von 1 bis diese Zahl werden Sie gerade Zahlen addieren (sumOfEvens)
+
+        // zweite method wird ein int Zahl bekommen und von 1 bis diese Zahl werden Sie ungerade Zahlen addieren(sumOfOdds)
+
+
+
+    }
+
+
+
+
+}
