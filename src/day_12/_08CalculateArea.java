32d75273a613a706d79a70eed627f2f52cfa3408
peace <mehmetakkaya1990@gmail.com>
2025-01-28 23:03:27 +0300

    return methods

---
+++ src/day_12/_08CalculateArea.java
@@ -0,0 +1,16 @@
+package day_12;
+
+public class _08CalculateArea {
+
+    public static void main(String[] args) {
+
+        // 3 calculateArea schreiben
+        // 1. M wird Quadrat Area berechnen = sideLenght * sideLenght (int sideLenght) ---> int
+        // 1. M wird Rechteck Area berechnen = lenght * width (int lenght, int width) ---> int
+        // 1. M wird Kreis Area berechnen = pi * radius * radius (double radius) ----> double
+
+    }
+
+
+
+}
