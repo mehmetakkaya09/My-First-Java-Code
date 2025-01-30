cae160d6b23cf7bda579afcc5c4ae6ea2418954c
peace <mehmetakkaya1990@gmail.com>
2025-01-30 22:05:51 +0300

    class and objects

---
+++ src/day_14/Kuchen.java
@@ -0,0 +1,11 @@
+package day_14;
+
+public class Kuchen {
+
+    String baecker;
+    int stuecke;
+    boolean mitEi;
+
+
+
+}
--- src/day_14/StudentImpl.java
+++ src/day_14/StudentImpl.java
@@ -12,9 +12,32 @@ public class StudentImpl {
         student1.gender = 'F';
         student1.getLessonInfo();
         student1.getHomeworkInfo();
+        student1.getHomeworkInfo();
         student1.printStudentInfo();
 
 
+        Student student2 = new Student();
+        student2.firstName = "Vadim";
+        student2.lastName = "Dokuchaev";
+        student2.age = 27;
+        student2.city = "Frankfurt";
+        student2.gender = 'M';
+        student2.getLessonInfo();
+        student2.getHomeworkInfo();
+        student2.eat("apple");
+        student2.printStudentInfo();
+
+        Student student3 = new Student();
+        student3.firstName = "Blagoja";
+        student3.lastName = "Andreevski";
+        student3.age = 46;
+        student3.city = "Walsrode";
+        student3.gender = 'M';
+        student3.getLessonInfo();
+        student3.getLessonInfo();
+        student3.getHomeworkInfo();
+        student3.drink("boza");
+        student3.printStudentInfo();
 
 
     }
