8b897692a063fafe1ab5a0148ca125242d3e3046
peace <mehmetakkaya1990@gmail.com>
2025-02-04 20:17:19 +0300

    String Pool

--- src/day_16/ScannerExp.java
+++ src/day_16/ScannerExp.java
@@ -1,5 +1,7 @@
 package day_16;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ScannerExp {
@@ -7,8 +9,20 @@ public class ScannerExp {
     public static void main(String[] args) {
 
         Scanner tastatur = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        int age = tastatur.nextInt();
 
+        System.out.print("Enter your name : ");
+        String name = tastatur.next(); //Enter
 
+        tastatur.nextLine();
+        System.out.print("Enter your address : ");
+        String address = tastatur.nextLine();
+
+        System.out.println("name = " + name);
+        System.out.println("address = " + address);
+
+        tastatur.close();
 
     }
 
--- src/day_16/person/Vehicle.java
+++ src/day_16/person/Vehicle.java
@@ -2,11 +2,11 @@ package day_16.person;
 
 public class Vehicle {
     // fields
-    String brandName;
-    String model;
-    int year;
-    double number;
-    static int count;
+    public String brandName;
+    public String model;
+    public int year;
+    public double number;
+    public static int count;
 
     public void finalize(){
         double number = 10; // local variable
