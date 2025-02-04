404cde7776bf9f64c7d78f6db27d74ab2a3af183
peace <mehmetakkaya1990@gmail.com>
2025-02-04 23:43:02 +0300

    Random, Math and Date Classes

---
+++ src/day_17/_05DateClass.java
@@ -0,0 +1,21 @@
+package day_17;
+
+public class _05DateClass {
+
+    public static void main(String[] args) {
+        // 3000
+        // Regression
+        // 3 Jahren
+        // 52 mal in einem Jahr
+        // 156
+        // erste Jahr hatten wir ein Bug in Recordings & Course Material - sdt20d
+        // wir haben dieses Bug gehandelt.
+        // 3. Jahr wurde nochmal gleiche Bug existiert.
+        // wir müssen Datums wissen -- wann haben wir das gehandelt. und wie haben wir das gemacht.
+
+
+
+    }
+
+
+}
