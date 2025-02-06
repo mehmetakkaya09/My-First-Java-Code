53f3ad4eab4338c426e01e65e4d8fbb6841defed
peace <mehmetakkaya1990@gmail.com>
2025-02-06 22:05:32 +0300

    String Methods

--- src/day_19/_02Contains.java
+++ src/day_19/_02Contains.java
@@ -23,10 +23,55 @@ public class _02Contains {
 
         if (isContains) {
             System.out.println("Du hast ein " + item);
-        }else
+        } else
             System.out.println("Du hast kein " + item);
 
+        //*-?!"
 
+
+//        System.out.print("Geben Ihre Vorname : ");
+//        String firstname = input.nextLine().trim();
+
+        // charAt() methode gibt immer ein char zurück
+        // contains() methode nimmt ein String
+        // wenn wir drin contains() method charAt() methode benutzen wollen, müssen wir diese charAt() zu String umwandeln.
+        // wenn wir nach ein char + "" schreiben dann können wie diese char als String benutzen
+
+
+        String specials = "*-?!\"";
+
+        boolean strSpecial = true;
+        boolean strLength = true;
+
+        do {
+            System.out.print("Geben Sie Ihr Vorname : ");
+            String vorname = input.nextLine().trim();
+            strSpecial = specialCharCheck(vorname, specials);
+            strLength = isLengthValid(vorname, 3);
+        } while (strSpecial || strLength);
+    }
+
+    public static boolean specialCharCheck(String str, String specials) {
+
+        boolean result = false;
+
+        for (int i = 0; i < specials.length(); i++) {
+            boolean isContain = str.contains(specials.charAt(i) + "");
+            if (isContain) {
+                System.out.println("Ihr Name beinhaltet " + specials.charAt(i));
+                result = true;
+                //break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean isLengthValid(String str, int length) {
+        boolean result = true;
+        if (str.length() >= length) {
+            result = false;
+        }
+        return result;
     }
 
 }
