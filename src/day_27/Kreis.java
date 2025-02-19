fe2ccfec3c965ab694e831e0c8946f6a8f0cc988
peace <mehmetakkaya1990@gmail.com>
2025-02-19 21:16:35 +0300

    ArrayLists

---
+++ src/day_27/Kreis.java
@@ -0,0 +1,47 @@
+package day_27;
+
+public class Kreis {
+
+    public double radius; // 0
+    public double höhe;
+
+    // driver
+
+
+
+    // Java erstellt immer in dem Hintergrund ein Konstruktor
+    // wir definieren nie ein return-type, nicht einmal void
+    // Standard/Parameterlose Konstruktor
+    public Kreis() {
+        radius = 3.0;
+    }
+
+    // Konstruktor Overloading ist immer möglich
+    public Kreis(double benutzerRadius){
+        radius = benutzerRadius;
+    }
+
+    // this schlüsselwort verwendet man damit class fields definieren.
+    public Kreis(double radius, double höhe){
+        this.radius = radius;
+        this.höhe = höhe;
+    }
+
+    public double getFlaeche(){
+        //double radius = 9;
+        return Math.pow(this.radius,2) * Math.PI;
+    }
+    // 2 * radius * pi
+    public double getUmfang(){
+        return 2 * this.radius * Math.PI;
+    }
+
+    // flaeche * höhe
+    public double getVolumen(){
+        return getFlaeche() * höhe;
+    }
+
+
+
+
+}
---
+++ src/day_27/KreisTest.java
@@ -0,0 +1,33 @@
+package day_27;
+
+public class KreisTest {
+
+    public static void main(String[] args) {
+
+        Kreis kreis = new Kreis();
+        kreis.höhe = 1;
+        System.out.println("kreis.höhe = " + kreis.höhe);
+
+        System.out.println("kreis.radius = " + kreis.radius);
+
+        System.out.println("kreis.getFlaeche() = " + kreis.getFlaeche());
+        System.out.println("kreis.getUmfang() = " + kreis.getUmfang());
+        System.out.println("kreis.getVolumen() = " + kreis.getVolumen());
+
+        Kreis kreis2 = new Kreis(2);
+
+        System.out.println("kreis2.radius = " + kreis2.radius);
+
+        System.out.println("kreis2.getFlaeche() = " + kreis2.getFlaeche());
+        System.out.println("kreis2.getUmfang() = " + kreis2.getUmfang());
+        System.out.println("kreis2.getVolumen() = " + kreis2.getVolumen());
+
+        Kreis kreis3 = new Kreis(5,7);
+
+        System.out.println("kreis3.höhe = " + kreis3.höhe);
+        System.out.println("kreis3.radius = " + kreis3.radius);
+        System.out.println("kreis3.getFlaeche() = " + kreis3.getFlaeche());
+        System.out.println("kreis3.getUmfang() = " + kreis3.getUmfang());
+        System.out.println("kreis3.getVolumen() = " + kreis3.getVolumen());
+    }
+}
