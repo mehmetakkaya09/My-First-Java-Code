7b97946b4429915f9f27eff72b33f73c79038559
peace <mehmetakkaya1990@gmail.com>
2025-02-26 22:38:57 +0300

    Encapsulation 2

---
+++ src/day_31/bank/Bank.java
@@ -0,0 +1,59 @@
+package day_31.bank;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String name;
+    private String adresse;
+
+    private ArrayList<Kunde> kunden;
+
+    public Bank(String name, String adresse) {
+        setName(name);
+        setAdresse(adresse);
+        kunden = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.trim();
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse.trim();
+    }
+
+    public void addKunde(Kunde kunde){
+        kunden.add(kunde);
+    }
+
+    public ArrayList<Kunde> getKunden() {
+        return new ArrayList<>(kunden);
+    }
+
+    @Override
+    public String toString() {
+        String str = "Bankname: " + name + "\n"
+                + "Bankadresse: " + adresse + "\n"
+                + "Kunden der Bank\n"
+                + "-------------------";
+
+        for (Kunde kunde : kunden) {
+            str += kunde + "\n";
+        }
+
+        return str;
+//        return "Bank{" +
+//                "name='" + name + '\'' +
+//                ", adresse='" + adresse + '\'' +
+//                '}';
+    }
+}
