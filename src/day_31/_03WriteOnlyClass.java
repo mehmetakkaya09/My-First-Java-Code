dc7e21ead8e1b0964337d3967d8e677c03e3ca30
peace <mehmetakkaya1990@gmail.com>
2025-02-26 20:58:27 +0300

    Encapsulation 2

---
+++ src/day_31/_03WriteOnlyClass.java
@@ -0,0 +1,22 @@
+package day_31;
+
+public class _03WriteOnlyClass {
+
+    private int value1;
+    private int value2;
+
+    public _03WriteOnlyClass(int value1, int value2) {
+        setValue1(value1);
+        setValue2(value2);
+    }
+
+    public void setValue1(int value1) {
+        this.value1 = value1;
+    }
+
+    public void setValue2(int value2) {
+        this.value2 = value2;
+    }
+
+
+}
---
+++ src/day_31/_04WriteOnlyTest.java
@@ -0,0 +1,16 @@
+package day_31;
+
+public class _04WriteOnlyTest {
+
+    public static void main(String[] args) {
+
+        _03WriteOnlyClass obj1 = new _03WriteOnlyClass(2,6);
+        _03WriteOnlyClass obj2 = new _03WriteOnlyClass(1,0);
+
+        obj1.setValue1(3);
+        obj2.setValue2(9);
+
+
+    }
+
+}
