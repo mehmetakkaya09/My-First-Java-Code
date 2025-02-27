3938a995f7be45b9cd13b7e5ee140e709de5e9f0
peace <mehmetakkaya1990@gmail.com>
2025-02-27 23:01:41 +0300

    static variables, methoden and blocks

---
+++ src/day_32/Rechner.java
@@ -0,0 +1,32 @@
+package day_32;
+
+public class Rechner {
+
+    int num1; // für objekten
+    int num2;
+
+    private Rechner(){}
+
+
+    // weil instances zur Objekten gehören, können sie in static methoden nicht benutzen
+    public static int addieren(int zahl1, int zahl2) {  // für alle Klasse
+        return zahl1 + zahl2;
+    }
+
+    public static int subtrahieren(int zahl1, int zahl2) {
+        return zahl1 - zahl2;
+    }
+    public static int multiplizieren(int zahl1, int zahl2) {
+        //Math.pow(2,4);
+        return zahl1 * zahl2;
+    }
+
+    public static int dividieren(int zahl1, int zahl2) {
+        return zahl1 / zahl2;
+    }
+
+
+
+
+
+}
---
+++ src/day_32/RechnerTest.java
@@ -0,0 +1,19 @@
+package day_32;
+
+public class RechnerTest {
+
+    public static void main(String[] args) {
+
+        //Rechner rechner = new Rechner();
+
+        System.out.println(Rechner.addieren(1, 2));
+        System.out.println(Rechner.subtrahieren(10, 2));
+        System.out.println(Rechner.multiplizieren(4, 2));
+        System.out.println(Rechner.dividieren(6, 2));
+
+
+    }
+
+
+
+}
