e28c408f7e28ee122b8ddcb4713627cb0b018907
peace <mehmetakkaya1990@gmail.com>
2025-03-10 22:03:27 +0300

    AccessModifiers

---
+++ src/day_37/packageB/ChildB.java
@@ -0,0 +1,27 @@
+package day_37.packageB;
+
+import day_37.packageA.AccessModifiers;
+
+public class ChildB extends AccessModifiers {
+
+    public void method() {
+        super.protectedModifierField = false;
+        super.protectedModifierMethod();
+    }
+
+    public static void main(String[] args) {
+
+        ChildB obj = new ChildB();
+
+        // Auch wenn beide Klasse in unterschiedliche Pakette befinden, weil es zwischen zwei klasse Inheritence Abhangigkeit gibt, kann man diese variablen und methoden zugreifen.
+        obj.protectedModifierField = true;
+
+        obj.protectedModifierMethod();
+
+        obj.publicModifierField = 3.9;
+        obj.publicModifierMethod();
+
+
+    }
+
+}
