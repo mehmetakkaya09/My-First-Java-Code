b45dbea5f97c186369273bd342acb9a4ffb9d1ff
peace <mehmetakkaya1990@gmail.com>
2025-03-19 20:57:18 +0300

    review and polymorphism

---
+++ src/day_43/browser/DriverTest.java
@@ -0,0 +1,60 @@
+package day_43.browser;
+
+import java.util.ArrayList;
+
+public class DriverTest {
+    public static void main(String[] args) {
+
+        //WebDriver chrome = new ChromeDriver();
+
+        RemoteWebDriver chrome = new ChromeDriver();
+
+        RemoteWebDriver firefox = new FirefoxDriver();
+
+        RemoteWebDriver edge = new EdgeDriver();
+
+        RemoteWebDriver safari = new SafariDriver();
+
+        RemoteWebDriver opera = new OperaDriver();
+
+        ArrayList<RemoteWebDriver> drivers = new ArrayList<>();
+        drivers.add(chrome);
+        drivers.add(firefox);
+        drivers.add(edge);
+        drivers.add(safari);
+        drivers.add(opera);
+
+        System.out.println();
+
+        for (RemoteWebDriver driver : drivers) {
+
+            System.out.println(driver.getBrowserName().name());
+            System.out.println();
+
+            driver.get("https://www.amazon.de/");
+
+            System.out.println(driver.getTitle());
+
+            driver.findElement("nav-global-location-popover-link");
+
+            driver.findElements("nav-belt");
+
+            driver.executeScript("window.scrollBy(0,250)");
+
+            driver.takeScreenShot();
+
+            driver.close();
+
+            driver.quit();
+
+            System.out.println("**************************************");
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}
