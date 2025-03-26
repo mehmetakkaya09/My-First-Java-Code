b8875e5977a45e98913102a6bb1b3f20f7894b40
peace <mehmetakkaya1990@gmail.com>
2025-03-26 22:39:27 +0300

    Maps

---
+++ src/day_47/maps/_01HashMap.java
@@ -0,0 +1,32 @@
+package day_47.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _01HashMap {
+    public static void main(String[] args) {
+          //Key DT   Value Datatype
+        Map<String, Double> salaries = new HashMap<>();
+
+        // Maps haben keine index
+        // Maps erlauben Duplikaten nicht.
+        // Keys müssen immer einzigartig sein.
+        // Wenn man ein Key zweimal definieren, kann man nur den Wert von Key verandern.
+                  //SCHLUSSEL     WERT
+                    //  KEY      VALUE
+        salaries.put("Blagoja", 50_000.0);
+        salaries.put("Blagoja", 70_000.0);
+        salaries.put("Süreyya", 60_000.0);
+        salaries.put("Vadim", 60_000.0);
+
+        System.out.println(salaries);
+
+        // um eine Key Information(Wert) einzulesen, benutzt man get() method
+        // get() method nimmt Key(Schlüssel), und gibt (Value) Wert zurück
+        System.out.println("salaries.get(\"Vadim\") = " + salaries.get("Vadim"));
+        System.out.println("salaries.get(\"Blagoja\") = " + salaries.get("Blagoja"));
+        System.out.println("salaries.get(\"Süreyya\") = " + salaries.get("Süreyya"));
+
+
+    }
+}
---
+++ src/day_47/maps/_02Players.java
@@ -0,0 +1,29 @@
+package day_47.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _02Players {
+
+    public static void main(String[] args) {
+
+        // In eine Fussball Team wie kann ich Spielernummer und Namen in einem Map speichern?
+
+        Map<Integer, String> spielersMap = new HashMap<>();
+
+        spielersMap.put(1,"De Gea");
+        spielersMap.put(10,"Messi");
+        spielersMap.put(9, "Ronaldo");
+        spielersMap.put(7,"Ronaldo");
+        spielersMap.put(11,"Neymar");
+
+        System.out.println("spielersMap.get(7) = " + spielersMap.get(7));
+        System.out.println("spielersMap.get(11) = " + spielersMap.get(11));
+        System.out.println("spielersMap.get(0) = " + spielersMap.get(0));
+        System.out.println("spielersMap.get(\"Benzema\") = " + spielersMap.get("Benzema"));
+
+
+    }
+
+
+}
