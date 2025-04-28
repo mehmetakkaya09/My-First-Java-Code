378c39a3df2632394514b081c304d906d77fba5e
peace <mehmetakkaya1990@gmail.com>
2025-04-28 21:09:06 +0300

    Stream API

--- src/day_52/StreamAPI.java
+++ src/day_52/StreamAPI.java
@@ -12,6 +12,8 @@ public class StreamAPI {
         List<Integer> list = new ArrayList<>(Arrays.asList(1,1,1,2,2,3,3,3,3,4,4,4,45,5,5,5,5,6,7,7,7,7,8));
         System.out.println(list);
 
+        //ohne Duplikaten
+        //ergebnisse müssen wir zu einer bestimmten Type umwandeln (Collection Type oder Array)
         list = list.stream().distinct().collect(Collectors.toList());
 
         System.out.println(list);
@@ -26,6 +28,8 @@ public class StreamAPI {
 
         List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         System.out.println(list2);
+
+        // ab n'th index kann man alle werten erhalten
         List<Integer> skipList = list2.stream().skip(5).collect(Collectors.toList());
         System.out.println(skipList);
 
@@ -34,7 +38,17 @@ public class StreamAPI {
         int[] skipArray = Arrays.stream(arr2).skip(3).toArray();
         System.out.println(Arrays.toString(skipArray));
 
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(10,4,2,3,4,5,6,7,8,9,10));
+        System.out.println(list3);
 
+        //ab n'th lange kann man etwas machen
+        List<Integer> limitList = list3.stream().limit(7).skip(4).collect(Collectors.toList());
+        System.out.println(limitList);
+
+        int[] arr3 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(arr3));
+        int[] limitArray = Arrays.stream(arr3).limit(5).skip(2).toArray();
+        System.out.println(Arrays.toString(limitArray));
 
 
     }
