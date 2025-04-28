8b7c30c55eab7e226a84fa731f5ee0548cdca0ed
peace <mehmetakkaya1990@gmail.com>
2025-04-28 21:53:29 +0300

    Stream API

--- src/day_52/StreamAPI.java
+++ src/day_52/StreamAPI.java
@@ -9,7 +9,7 @@ public class StreamAPI {
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>(Arrays.asList(1,1,1,2,2,3,3,3,3,4,4,4,45,5,5,5,5,6,7,7,7,7,8));
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 45, 5, 5, 5, 5, 6, 7, 7, 7, 7, 8));
         System.out.println(list);
 
         //ohne Duplikaten
@@ -18,7 +18,7 @@ public class StreamAPI {
 
         System.out.println(list);
 
-        int[] arr = {1,1,1,2,2,3,3,3,3,4,4,4,45,5,5,5,5,6,7,7,7,7,8};
+        int[] arr = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 45, 5, 5, 5, 5, 6, 7, 7, 7, 7, 8};
 
         System.out.println(Arrays.toString(arr));
 
@@ -26,31 +26,31 @@ public class StreamAPI {
 
         System.out.println(Arrays.toString(resArr));
 
-        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         System.out.println(list2);
 
         // ab n'th index kann man alle werten erhalten
         List<Integer> skipList = list2.stream().skip(5).collect(Collectors.toList());
         System.out.println(skipList);
 
-        int[] arr2= {1,2,3,4,5,6,7,8,9,10};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(Arrays.toString(arr2));
         int[] skipArray = Arrays.stream(arr2).skip(3).toArray();
         System.out.println(Arrays.toString(skipArray));
 
-        List<Integer> list3 = new ArrayList<>(Arrays.asList(10,4,2,3,4,5,6,7,8,9,10));
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(10, 4, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         System.out.println(list3);
 
         //ab n'th lange kann man etwas machen
         List<Integer> limitList = list3.stream().limit(7).skip(4).collect(Collectors.toList());
         System.out.println(limitList);
 
-        int[] arr3 = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(Arrays.toString(arr3));
         int[] limitArray = Arrays.stream(arr3).limit(5).skip(2).toArray();
         System.out.println(Arrays.toString(limitArray));
 
-        List<Integer> list4 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        List<Integer> list4 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
         List<Integer> mapList = list4.stream().map(num -> num * 10).toList();
         System.out.println(mapList);
 
@@ -58,6 +58,16 @@ public class StreamAPI {
         System.out.println(names);
         List<String> mapTrim = names.stream().map(name -> name.trim()).toList();
         System.out.println(mapTrim);
+
+        List<String> surnames = new ArrayList<>(Arrays.asList("Jack", "     Yılmaz", "Lee     ", "Jo", "x", "", "               "));
+
+        System.out.println(surnames);
+        List<String> passedSurnames = surnames.stream().filter(surname -> surname.trim().length() > 1).toList();
+        System.out.println(passedSurnames);
+
+        passedSurnames = passedSurnames.stream().map(s -> s.trim()).toList();
+        System.out.println(passedSurnames);
+
 
 
 
